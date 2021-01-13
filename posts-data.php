e801977a8f0d26160e2377f15079ab6f7b433536
broker <8497915@gmail.com>
2021-01-13 15:51:18 +0200

    Add posts array-data. PHP treinee #5

---
+++ posts-data.php
@@ -0,0 +1,29 @@
+<?php
+
+$postsData = [
+    [
+        "title" => "Post title 1",
+        "link" => "#",
+        "description" => "Post 1 description post description post description ...",
+    ],
+    [
+        "title" => "Post title 2",
+        "link" => "#",
+        "description" => "Post 2 description post description post description ...",
+    ],
+    [
+        "title" => "Post title 3",
+        "link" => "#",
+        "description" => "Post 3 description post description post description ...",
+    ],
+    [
+        "title" => "Post title 4",
+        "link" => "#",
+        "description" => "Post 4 description post description post description ...",
+    ],
+    [
+        "title" => "Post title 5",
+        "link" => "#",
+        "description" => "Post 5 description post description post description ...",
+    ],
+];
